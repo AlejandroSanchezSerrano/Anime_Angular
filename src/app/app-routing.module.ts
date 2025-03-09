a2f82621fc88fe0d79d50e8eb01a01f9e54dc7d1
DESKTOP-SU5B02H\Alejandro <alejandro.sanchez-serrano@iesruizgijon.com>
2025-03-09 13:09:39 +0100

    Estilo de /inicio y app.component ready

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { Error404Component } from './components/error404/error404.component';
 
 const routes: Routes = [
   { path: 'inicio', component: InicioComponent },
-  { path: '', redirectTo: '/inicio', pathMatch: 'full' }
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  { path: '**', component: Error404Component }
 ];
 
 @NgModule({
